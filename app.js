35b804d0e98baf943a515a007d5e2b8edcc880e6
Pavel Mezencev <mezencev24@mail.ru>
2023-11-29 13:57:05 +0300

    initial

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+const indexRoute = require('./routes/users');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/user', indexRoute);
+
+app.listen(PORT, () => console.log(`Port ${PORT}`));
